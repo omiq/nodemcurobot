0b23c8b9192ca8cdffe4ab0876e54f1d65e86a8f
omiq <chris@chrisg.com>
2019-02-06 16:14:36 -0700

    counter

--- websockets/websockets.py
+++ websockets/websockets.py
@@ -1,27 +1,44 @@
 from ws_connection import ClientClosedError
+
 from ws_server import WebSocketServer, WebSocketClient
 
 
 class TestClient(WebSocketClient):
+
     def __init__(self, conn):
+
         super().__init__(conn)
+        self.counter = 0
 
     def process(self):
+
         try:
+
             msg = self.connection.read()
+
             if not msg:
                 return
+
             msg = msg.decode("utf-8")
+
             items = msg.split(" ")
+
             cmd = items[0]
-            if cmd == "Hello":
-                self.connection.write(cmd + " World")
-                print("Hello World")
+
+            if cmd != "":
+                self.counter += 1
+
+                self.connection.write(cmd + str(self.counter))
+
+                print("Responded")
+
         except ClientClosedError:
+
             self.connection.close()
 
 
 class TestServer(WebSocketServer):
+
     def __init__(self):
         super().__init__("sockets.txt", 2)
 
@@ -30,10 +47,18 @@ class TestServer(WebSocketServer):
 
 
 server = TestServer()
+
 server.start()
+
 try:
+
     while True:
         server.process_all()
+
 except KeyboardInterrupt:
+
     pass
+
 server.stop()
+
+
