7de93fcef834241304f48c6686421d63baed743a
omiq <chris@chrisg.com>
2019-02-15 16:52:14 -0700

    rename

--- put.sh
+++ put.sh
@@ -9,8 +9,12 @@ ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put wifi.txt
 ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put success.txt
 ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put http_server.py
 
-# directory
-ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put websockets
+# robot control
+ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put websockets.py
+ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put sockets.html
+ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put ws_connection.py
+ampy -d 0.5 -p /dev/tty.SLAB_USBtoUART put ws_server.py
+
 
 
 
