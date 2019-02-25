4e98fd0aeacc2d05b2e99546bb9335f1bf914d70
omiq <chris@chrisg.com>
2019-02-25 16:12:26 -0700

    ben's tweaks

--- motorcontrol.py
+++ motorcontrol.py
@@ -2,10 +2,10 @@ from machine import Pin, PWM
 import time
 
 """ nodemcu pins from the motor shield """
-pin1 = Pin(5, Pin.OUT)  # D1
-pin2 = Pin(4, Pin.OUT)  # D2
-pin3 = Pin(0, Pin.OUT)  # D3
-pin4 = Pin(2, Pin.OUT)  # D4
+pin1 = Pin(5, Pin.OUT)  # D1 Direction
+pin2 = Pin(4, Pin.OUT)  # D2 Direction
+pin3 = Pin(0, Pin.OUT)  # D3 Speed
+pin4 = Pin(2, Pin.OUT)  # D4 Speed
 
 """ named after the L9110 h-bridge pins """
 BIN1 = PWM(pin1, freq=750)
@@ -14,7 +14,9 @@ AIN1 = PWM(pin2, freq=750)
 AIN2 = PWM(pin4, freq=750)
 
 """ TODO: variable speed """
-speed = 950
+speed = 1000
+fwd = 0
+rev = 1023
 
 
 def stop_all():
@@ -23,31 +25,48 @@ def stop_all():
 
 
 def backward():
-    BIN1.duty(0)
-    BIN2.duty(speed)
-    AIN1.duty(0)
-    AIN2.duty(speed)
+    print("Backward")
+    BIN1.duty(speed)
+    BIN2.duty(rev)
+    AIN1.duty(speed)
+    AIN2.duty(rev)
 
 
 def forward():
     print("Forward")
     BIN1.duty(speed)
-    BIN2.duty(0)
+    BIN2.duty(fwd)
     AIN1.duty(speed)
-    AIN2.duty(0)
+    AIN2.duty(fwd)
+
+
+def hardleft():
+    print("HardLeft")
+    BIN1.duty(speed)
+    BIN2.duty(fwd)
+    AIN1.duty(speed)
+    AIN2.duty(rev)
 
 
 def left():
+    print("Left")
     BIN1.duty(speed)
-    BIN2.duty(0)
+    BIN2.duty(fwd)
     AIN1.duty(0)
-    AIN2.duty(speed)
-
-
-def right():
-    BIN1.duty(0)
-    BIN2.duty(speed)
-    AIN1.duty(speed)
     AIN2.duty(0)
 
 
+def right():
+    print("Right")
+    BIN1.duty(0)
+    BIN2.duty(0)
+    AIN1.duty(speed)
+    AIN2.duty(fwd)
+
+
+def hardright():
+    print("HardRight")
+    BIN1.duty(speed)
+    BIN2.duty(rev)
+    AIN1.duty(speed)
+    AIN2.duty(fwd)
